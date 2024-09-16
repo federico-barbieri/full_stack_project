43386ba5201df82bbed56c1a04f3b3556a5db77a
Federico Barbieri <federicobarbieri54@gmail.com>
2024-09-16 11:03:03 +0200

    card

--- fullstack_project/src/components/GameCard.tsx
+++ fullstack_project/src/components/GameCard.tsx
@@ -2,27 +2,23 @@ import React from 'react'
 import {Card, Image, Heading, CardBody, Text, HStack} from "@chakra-ui/react";
 import PlatformIconsList from "./PlatformIconsList";
 import CriticScore from "./CriticScore";
-import {Game} from "../hooks/useGames";
+import { Game } from '../hooks/useGames';
 
-export interface Props{
+export interface Props {
     game: Game;
-}
-
-const GameCard = ({game}: Props) => {
-
-  return (
-    <Card>
-        <Image src={game.background_image} />
+  }
+  
+  const GameCard = ({ game }: Props) => {
+    return (
+      <Card>
+        <Image src={game.background_image} alt={game.name} />
         <CardBody>
-            <Heading fontSize="2x1">{game.name}</Heading>
-            <PlatformIconsList 
-            platforms={game.parent_platforms.map((pp) => pp.platform)} 
-            />
-            <CriticScore score={game.metacritic} />
+          <Heading fontSize="2xl">{game.name}</Heading>
+          <PlatformIconsList platforms={game.parent_platforms.map((pp) => pp.platform)} />
+          <CriticScore score={game.metacritic} />
         </CardBody>
-
-    </Card>
-  )
-}
-
-export default GameCard
+      </Card>
+    );
+  };
+  
+  export default GameCard;
