2c93b200cb1c769b2e35e31d38f47138f71004e7
Federico Barbieri <federicobarbieri54@gmail.com>
2024-09-16 10:15:52 +0200

    icons

--- fullstack_project/src/components/GameCard.tsx
+++ fullstack_project/src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Card, Image, Heading, CardBody} from "@chakra-ui/react";
+import {Card, Image, Heading, CardBody, Text} from "@chakra-ui/react";
+import PlatformIconsList from "./PlatformIconsList";
 
 interface Props{
     game: Game;
@@ -12,6 +13,9 @@ function GameCard({game}: Props) {
         <Image src={game.background_image} />
         <CardBody>
             <Heading fontSize="2x1">{game.name}</Heading>
+            <PlatformIconsList 
+            platforms={game.parent_platforms.map((pp) => pp.platform)} 
+            />
         </CardBody>
 
     </Card>
---
+++ fullstack_project/src/components/PlatformIconsList.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import {Text, Icon, HStack} from "@chakra-ui/react"
+
+import {
+    FaWindows,
+    FaXbox,
+    FaPlaystation,
+    FaApple,
+    FaLinux,
+    FaAndroid,
+    FaNintendoSwitch,
+    FaSteam,
+    FaMacos,
+    FaIos,
+    FaWeb,
+} from "react-icons/fa";
+
+import {MdPhoneIphone} from "react-icons/md";
+import {SiNintendo} from "react-icons/si";
+import {BsGlobe} from "react-icons/bs";
+
+interface Props {
+    platforms: Platform [];
+}
+
+function PlatformIconsList({platforms}: Props) {
+
+    const getIcon = (slug: string) => {
+        switch (slug) {
+            case 'windows':
+                return FaWindows;
+                case 'xbox':
+                    return FaXbox;
+                    case 'playstation':
+                        return FaPlaystation;
+                        case 'apple':
+                            return FaApple;
+                            case 'linux':
+                                return FaLinux;
+                                case 'android':
+                                    return FaAndroid;
+                                        case 'steam':
+                                            return FaSteam;
+                                            case 'nintendo':
+                                                return SiNintendo;
+        }
+}
+                                                        
+    
+
+
+
+  return (
+    <HStack marginY="2">
+        {platforms.map((platform) => (
+        <Icon key={platform.id} as={getIcon(platform.slug)} />
+
+    ))}
+
+    </HStack>
+   
+  )
+}
+
+export default PlatformIconsList
